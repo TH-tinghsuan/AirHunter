46fd9421afc8c4c8291e297a5eadc71bdf11d154
Ting Hsuan Lin <tinghsuan1998@gmail.com>
2023-09-27 18:34:34 +0800

    add: login/logout/register function

--- App/server/controllers/search_controller.py
+++ App/server/controllers/search_controller.py
@@ -1,23 +1,24 @@
 from server import app
-from server.models.flights import get_search_result, get_airport_detail
-from flask import jsonify, request,render_template
+#from server.models.flights import get_search_result, get_airport_detail
+from flask import jsonify, request,render_template, redirect
+from server.models.flight import get_flights_info, get_airport_detail
 
 @app.route("/")
-def hello_world():
-    return "Hello, MVC."
-
+def index():
+    return render_template("index.html")
+        
 @app.route("/search", methods=["GET"])
 def search_price():
     return render_template("search.html")
 
 
 @app.route("/flight/lists", methods=["GET"])
-def search_api():
+def get_flight_lists():
     if request.args.get("departureAirports") and request.args.get("arriveAirports") and request.args.get("departureDates"):
         departureAirports = request.args.get("departureAirports")
         arriveAirports = request.args.get("arriveAirports")
         departureDates = request.args.get("departureDates")
-        flight_info = get_search_result(arriveAirports, departureAirports, departureDates)
+        flight_info = get_flights_info(arriveAirports, departureAirports, departureDates)
         if flight_info != "No data":
             return render_template("outcome.html", flight_info=flight_info, depart=get_airport_detail(departureAirports), arrive=get_airport_detail(arriveAirports), date=departureDates)
         else:
@@ -25,4 +26,3 @@ def search_api():
     else:
         return "Bad request"
     
-
