aa33759d4fa83ddc54e35a76c20029e51b00fe73
Ting Hsuan Lin <tinghsuan1998@gmail.com>
2023-10-19 10:28:35 +0800

    fix: notification for airflow taks

--- Airflow/dags/crawler_all_dag.py
+++ Airflow/dags/crawler_all_dag.py
@@ -5,7 +5,8 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.models import Variable
 from datetime import datetime, timedelta
 from modules.crawler import lifetour_scraper, richmond_crawler, ezTravel_crawler, ezFly_crawler
-from modules.transfer import gernerate_batch_version, insert_to_main_table, get_old_price_history_data, insert_to_price_history, compare_price_difference, send_notification
+from modules.transfer import insert_to_main_table, get_old_price_history_data, insert_to_price_history, compare_price_difference, send_notification
+from modules.notification import send_noti_email
 
 class TaskID:
     start_task_id = "start_task"
@@ -32,13 +33,39 @@ def get_utc_8_date(**kwargs):
     utc_8_date = utc_date + timedelta(hours=8)
     formatted_date = utc_8_date.strftime('%Y-%m-%d')
     Variable.set("search_date_key", formatted_date)
+
+def notify_on_failure(context):
+    ti = context['ti']
+    task_id = ti.task_id
+    dag_id = ti.dag_id
+    execution_date = ti.execution_date
+    email_content = f"""Hi, 
+                        Task ID {task_id} has error. Please check.
+                        executed date: {execution_date}
+                        dag ID: {dag_id}
+                        Thank You."""
+    email_subject = f"Airflow notification-task_id: {task_id} error occur."
+    send_noti_email(email_subject, email_content, "tinghsuan1998@gmail.com")
+
+def notify_on_success(context):
+    ti = context['ti']
+    task_id = ti.task_id
+    dag_id = ti.dag_id
+    execution_date = ti.execution_date
+    email_content = f"""Hi, 
+                        Task ID {task_id} has successfully executed.
+                        executed date: {execution_date}
+                        dag ID: {dag_id}
+                        Thank You."""
+    email_subject = f"Airflow notification-task_id: {task_id} job done."
+    send_noti_email(email_subject, email_content, "tinghsuan1998@gmail.com")
     
 
 utc_date = datetime.utcnow()
 utc_8_date = utc_date + timedelta(hours=8)
 current_date = utc_8_date.date() + timedelta(days=1)
     
-@dag(start_date=datetime(2023, 10, 1), catchup=False, schedule_interval=timedelta(hours=4), tags=['data_pipeline'])
+@dag(start_date=datetime(2023, 10, 1), catchup=False, schedule_interval=timedelta(hours=8), tags=['data_pipeline'])
 def crawler_dag_all_test():
     start_task = EmptyOperator(task_id=TaskID.start_task_id)
 
@@ -48,80 +75,96 @@ def crawler_dag_all_test():
     
     lifeTour_crawl_task_1 = PythonOperator(task_id=TaskID.lf_crawler_task_1_id,
                                 python_callable=lifetour_scraper,
+                                on_failure_callback=notify_on_failure,
                                 provide_context=True, 
                                 op_kwargs = {"start_date": current_date, "total_dates": 20})
     
     lifeTour_crawl_task_2 = PythonOperator(task_id=TaskID.lf_crawler_task_2_id,
                                 python_callable=lifetour_scraper, 
+                                on_failure_callback=notify_on_failure,
                                 provide_context=True,
                                 op_kwargs = {"start_date": current_date + timedelta(days=20), "total_dates": 20})
     
     lifeTour_crawl_task_3 = PythonOperator(task_id=TaskID.lf_crawler_task_3_id,
                                 python_callable=lifetour_scraper, 
+                                on_failure_callback=notify_on_failure,
                                 provide_context=True,
                                 op_kwargs = {"start_date": current_date + timedelta(days=40), "total_dates": 21})
     
     richmond_crawl_task_1 = PythonOperator(task_id=TaskID.rh_crawler_task_1_id,
                                 python_callable=richmond_crawler, 
+                                on_failure_callback=notify_on_failure,
                                 provide_context=True,
                                 op_kwargs = {"start_date": current_date, "total_dates": 20})
     
     richmond_crawl_task_2 = PythonOperator(task_id=TaskID.rh_crawler_task_2_id,
                                 python_callable=richmond_crawler,
+                                on_failure_callback=notify_on_failure,
                                 provide_context=True, 
                                 op_kwargs = {"start_date": current_date + timedelta(days=20), "total_dates": 20})
     
     richmond_crawl_task_3 = PythonOperator(task_id=TaskID.rh_crawler_task_3_id,
                                 python_callable=richmond_crawler, 
+                                on_failure_callback=notify_on_failure,
                                 provide_context=True,
                                 op_kwargs = {"start_date": current_date + timedelta(days=40), "total_dates": 21})
     
     ezTravel_crawl_task_1 = PythonOperator(task_id=TaskID.ezT_crawler_task_1_id,
                                 python_callable=ezTravel_crawler, 
+                                on_failure_callback=notify_on_failure,
                                 provide_context=True,
                                 op_kwargs = {"start_date": current_date, "total_dates": 60})
    
     ezFly_crawl_task_1 = PythonOperator(task_id=TaskID.ezF_crawler_task_1_id,
                                 python_callable=ezFly_crawler, 
+                                on_failure_callback=notify_on_failure,
                                 provide_context=True,
                                 op_kwargs = {"start_date": current_date, "total_dates": 20})
     
     ezFly_crawl_task_2 = PythonOperator(task_id=TaskID.ezF_crawler_task_2_id,
                                 python_callable=ezFly_crawler,
+                                on_failure_callback=notify_on_failure,
                                 provide_context=True, 
                                 op_kwargs = {"start_date": current_date + timedelta(days=20), "total_dates": 20})
     
     ezFly_crawl_task_3 = PythonOperator(task_id=TaskID.ezF_crawler_task_3_id,
                                 python_callable=ezFly_crawler, 
+                                on_failure_callback=notify_on_failure,
                                 provide_context=True,
                                 op_kwargs = {"start_date": current_date + timedelta(days=40), "total_dates": 21})
 
     insert_to_main_table_task = PythonOperator(task_id=TaskID.insert_to_main_table_task_id,
                                 python_callable=insert_to_main_table,
+                                on_failure_callback=notify_on_failure,
                                 retries=3,
                                 retry_delay=timedelta(minutes=1))
 
     get_old_price_history_task = PythonOperator(task_id=TaskID.get_old_price_history_task_id,
                                 python_callable=get_old_price_history_data,
                                 provide_context=True,
+                                on_failure_callback=notify_on_failure,
                                 retries=3,
                                 retry_delay=timedelta(minutes=1))
 
     insert_to_price_history_task = PythonOperator(task_id=TaskID.insert_to_price_history_task_id,
                                 python_callable=insert_to_price_history,
                                 provide_context=True,
+                                on_failure_callback=notify_on_failure,
                                 retries=3,
                                 retry_delay=timedelta(minutes=1))
 
     compare_price_diff_task = PythonOperator(task_id=TaskID.compare_price_diff_task_id,
                                 python_callable=compare_price_difference,
                                 provide_context=True,
+                                on_failure_callback=notify_on_failure,
                                 retries=3,
                                 retry_delay=timedelta(minutes=1))
 
     send_price_notification_task = PythonOperator(task_id=TaskID.send_price_notification_task_id,
                                 python_callable=send_notification,
                                 provide_context=True,
+                                on_failure_callback=notify_on_failure,
+                                on_success_callback=notify_on_success,
                                 retries=3,
                                 retry_delay=timedelta(minutes=1))
 
--- Airflow/dags/modules/crawler.py
+++ Airflow/dags/modules/crawler.py
@@ -4,7 +4,7 @@ import json
 import concurrent.futures
 from modules.cloud import upload_to_s3
 from airflow.models import Variable
-
+import random
 
 def get_all_by_date(date, date_format, crawler_func):
     bundle = {"TSA":["TTT", "MZG", "HUN", "KNH", "MFK", "LZN"],
@@ -104,7 +104,8 @@ def ezTravel_crawler(start_date, total_dates):
             if data["data"]:
                 return data["data"]
         else:
-            print("error, ", r.status_code)
+            message = r.status_code
+            print(f"error, {message}")
     search_date = Variable.get("search_date_key")  
     date_list = [start_date + timedelta(days=i) for i in range(total_dates)]
     for date in date_list:
@@ -134,14 +135,31 @@ def ezFly_crawler(start_date, total_dates):
                     "Sdate": date,
                     "Edate": date,
                     "Guid": guid}
-        response = requests.post(url="https://ea.ezfly.com/ProdDAIR/Home/GetProdLists", data=form_data)
+        my_headers = [
+        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14",
+        "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)",
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
+        'Opera/9.25 (Windows NT 5.1; U; en)',
+        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+        'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
+        'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+        'Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
+        "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7",
+        "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0 "
+        ]
+        headers = {'User-Agent':random.choice(my_headers)}
+        response = requests.post(url="https://ea.ezfly.com/ProdDAIR/Home/GetProdLists", data=form_data, headers=headers)
         if response.status_code == 200:
             data = response.text
             if data:
                 data_dict = {"depart": depart_airport, "arrive": arrive_airport, "date": date, "info": data}
                 return data_dict
         else:
-            print("error." + response.status_code)
+            message = response.status_code
+            print(f"error, {message}")
     search_date = Variable.get("search_date_key") 
     date_list = [start_date + timedelta(days=i) for i in range(total_dates)]
     for date in date_list:
